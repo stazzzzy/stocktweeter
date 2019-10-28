82cdfcd310ea6ba252fc7166f98d79fe155ee8b9
stazzzzy <56558919+stazzzzy@users.noreply.github.com>
2019-10-28 17:27:47 -0400

    Update
    Changed HTML look
    Some functionality with ticker input
    Changed graphing libraries, this one is built for stock prices.

--- ticker_input.js
+++ ticker_input.js
@@ -1,9 +1,10 @@
-var stocks = new Stocks('L0DDAMG0NGJFSJEY');
-
-var options = {
-  symbol: 'AAPL',
-  interval: 'weekly',
-  amount: 52
-};
-
-var result = await stocks.timeSeries(options);
+function getticker(string){
+$.getJSON('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=5min&apikey=L0DDAMG0NGJFSJEY', function(data) { 
+			console.log(string);
+			var text = `Ticker: ${data['Meta Data']["2. Symbol"]}<br>
+						T-Zone: ${data['Meta Data']["6. Time Zone"]}`
+			
+			$(".info").html(text);
+		});
+	}
+	

eec4fc9a0b80ced3328673663feb2f537bae6a90
Tristan <stazzzzy@gmail.com>
2019-10-28 21:01:41 -0400

    Fix duplicating in file endings

--- ticker_input.js
+++ ticker_input.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function getticker(string){
 $.getJSON('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=5min&apikey=L0DDAMG0NGJFSJEY', function(data) { 
 			console.log(string);
@@ -8,15 +8,3 @@ $.getJSON('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbo
 			$(".info").html(text);
 		});
 	}
-=======
-function getticker(string){
-$.getJSON('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=5min&apikey=L0DDAMG0NGJFSJEY', function(data) { 
-			console.log(string);
-			var text = `Ticker: ${data['Meta Data']["2. Symbol"]}<br>
-						T-Zone: ${data['Meta Data']["6. Time Zone"]}`
-			
-			$(".info").html(text);
-		});
-	}
->>>>>>> 82cdfcd310ea6ba252fc7166f98d79fe155ee8b9
-	

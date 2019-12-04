4b4a7f262c2f9aff35d1f38e07454b124bc4016e
David Reider <freakdj1997@yahoo.com>
2019-12-04 15:13:57 -0500

    Added Trump Timeline to Column 1. Cleaned some HTML

--- ticker_input.js
+++ ticker_input.js
@@ -2,12 +2,12 @@ function getticker(string){
 	// Build URL for request to the API
 	let req = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + string.toUpperCase() + '&interval=5min&apikey=L0DDAMG0NGJFSJEY';
 	// JQuery function to retrieve the JSON from Alpha Vantage
-	$.getJSON(req, function(data) { 
+	$.getJSON(req, function(data) {
 			console.log(string);
 			var text = `Ticker: ${data['Meta Data']["2. Symbol"]}<br>
 						T-Zone: ${data['Meta Data']["6. Time Zone"]}`
 			$(".info").html(text); // Append JSON text to site HTML
+
 			set_chart_data(req);
 		});
-
 	}

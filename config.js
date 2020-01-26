c25efe996ce01cf83711d45acade92360400eb82
David Reider <freakdj1997@yahoo.com>
2020-01-25 21:19:04 -0500

    Reverting Merge

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'SHl26na5bOXi5ezojsMnmY5d2',
+  consumer_secret:      'gQfWRJ5JpBfp7VBWE53MBEL6hUFAIuIUVuKmqjvg4rWxFtnTiW',
+  access_token:         '785901517-fgDYo5u4iYbaUZWFdgiuiQe8Xg9vesE3KLy9jfhM',
+  access_token_secret:  'KEPLfajhjzmhcJQ131zw3VBa3lfAZoK8J7OhJZIweb2Nq'
+}

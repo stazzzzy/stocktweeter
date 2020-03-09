439ddd094011f5842c03f468281e4d49625e9046
David Reider <freakdj1997@yahoo.com>
2020-03-09 16:42:21 -0400

    Load flagged array

--- analysis.js
+++ analysis.js
@@ -14,6 +14,7 @@
 
 function a(time_series, threshold) { // Threshhold should be a float (i.e. .05 = 5%) time_series is an array of key value pairs
 	var flagged = []; 				 // Array of times that will have a trump tweet linked to them
+	
 	$.getJSON("https://firebasestorage.googleapis.com/v0/b/tweetdata-f8f42.appspot.com/o/tweets.json?alt=media&token=e2ce4a33-6996-4c54-84bb-1b6737b8b2f6", function(data) {
 			console.log("Test here");
 			console.log(data[0].created_at);
@@ -26,6 +27,7 @@ function a(time_series, threshold) { // Threshhold should be a float (i.e. .05 =
 				flagged.push(formatted_date); // NOT SURE IF THIS WORKS
 			}
 		});
+
 	for (var key in time_series) {
 		let cur = time_series[key];
 		let next = time_series[key+1];
